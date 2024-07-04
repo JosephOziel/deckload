e4f4d28bc563cc2bae716de96327e6bf39a80f24
Proloy Mishra <namitapro0987@gmail.com>
2024-07-04 14:01:18 +0530

    remove unnecessary functions in boilerplate code

--- backend-c/boilerplate.c
+++ backend-c/boilerplate.c
@@ -88,22 +88,6 @@ Func shallow_clone(Func f) {
     return f;
 }
 
-Vec clone(Vec v) {
-    Func* buf = new_buf(v.len*sizeof(FUNC));
-    for(size_t i=0; i<v.len; ++i)
-        buf[i] = shallow_clone(v.mem[i]);
-
-    size_t* rc = new_buf(sizeof(size_t));
-    *rc=1;
-    
-    return (Vec) {
-        .refcount=rc,
-        .mem=buf,
-        .len=v.len,
-        .cap=v.len,
-    };
-}
-
 void vec_push(Vec* s, Func f) {
     s->mem=ensure(s->mem, s->len+1, &s->cap, sizeof(Func));
     s->mem[s->len++]=f;
@@ -116,12 +100,6 @@ void vec_dec_len(Vec* s, size_t d) {
 
 size_t vec_len(Vec s) {
     return s.len;
-}
-
-Func vec_pop(Vec* s) {
-    assert(s->len>0);
-
-    return s->mem[--s->len];
 }
 
 Func vec_last(Vec s, size_t idx) {
@@ -146,14 +124,6 @@ void vec_append_except_last(Vec* v1, Vec v2) {
     v2.len++;
 }
 
-// Func flatten(Func f) {
-//     if(f.ty!=BLOCK) return f;
-//     Func c;
-//     while((c=vec_pop(&f.data.block)).ty == BLOCK) vec_append(&f.data.block, c);
-//     vec_push(&f.data.block, c);
-//     return f;
-// }
-
 void vec_into_block(Vec* v, size_t len) {
     assert(v->len>=len);
     if (len==1) return;
@@ -168,18 +138,6 @@ void vec_into_block(Vec* v, size_t len) {
             .block = (Vec) { p, rc, len, len }
         }
     });
-}
-
-int eq(Func a, Func b) {
-    if(a.ty!=b.ty) return 0;
-    if(a.ty==FUNC) return a.data.func == b.data.func;
-    assert(a.ty==BLOCK);
-    size_t len = vec_len(a.data.block);
-    if(len!=vec_len(b.data.block)) return 0;
-    for(size_t i=0;i<len;i++)
-        if(!eq(vec_get(a.data.block, i),vec_get(b.data.block, i)))
-            return 0;
-    return 1;
 }
 
 void d_call(Func f, Vec* s, size_t arg_start) {
